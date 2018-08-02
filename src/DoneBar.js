0801bc97ecf83a8a7c1abd1530cc19c7cfab0a75
Kris <km@balancair.com>
2018-08-02 13:53:56 +0200

    added colors to props. changed default backgroundColor to more accurate color that matches the current keyboard color

--- src/DoneBar.js
+++ src/DoneBar.js
@@ -1,119 +1,134 @@
 import React from 'react';
 import {
-  View,
-  TouchableOpacity,
-  Text,
-  Keyboard,
-  Animated,
-  StyleSheet,
-  LayoutAnimation,
-  Dimensions,
-  Platform
+    View,
+    TouchableOpacity,
+    Text,
+    Keyboard,
+    Animated,
+    StyleSheet,
+    LayoutAnimation,
+    Dimensions,
+    Platform
 } from 'react-native';
 import PropTypes from 'prop-types';
 
 let { width, height } = Dimensions.get('window');
 
 class DoneBar extends React.Component {
-  static propTypes = {
-    keyboardType: PropTypes.string,
-    includeLayoutAnimation: PropTypes.bool,
-    text: PropTypes.string,
-    onDone: PropTypes.func
-  }
-
-  static defaultProps = {
-    keyboardType: 'numeric',
-    includeLayoutAnimation: true,
-    text: 'Done',
-    onDone: () => {}
-  }
-
-  state = {
-    height: 0,
-    width: width,
-    bottom: -81
-  }
-
-  componentWillMount(props) {
-    const config = {
-      duration: 250,
-      update: {
-        duration: 250,
-        type: LayoutAnimation.Types.keyboard
-      }
+    static propTypes = {
+        borderColor: PropTypes.string,
+        backgroundColor: PropTypes.color,
+        keyboardType: PropTypes.string,
+        includeLayoutAnimation: PropTypes.bool,
+        text: PropTypes.string,
+        onDone: PropTypes.func,
     };
 
-    this.keyboardWillChangeFrameListener = Keyboard.addListener('keyboardWillChangeFrame', ({ endCoordinates }) => {
-      let { screenY } = endCoordinates;
+    static defaultProps = {
+        keyboardType: 'numeric',
+        includeLayoutAnimation: true,
+        text: 'Done',
+        onDone: () => {},
+        borderColor: null,
+        backgroundColor: '#D3D5DB',
+    };
 
-      if (screenY === height || this.props.keyboardType !== 'numeric') {
-        bottom = -81;
-      } else {
-        bottom = endCoordinates.height - 40;
-      }
+    state = {
+        height: 0,
+        width: width,
+        bottom: -81
+    };
 
-      this.props.includeLayoutAnimation ? LayoutAnimation.configureNext(config) : null;
-      this.setState({
-        bottom,
-        width: endCoordinates.width
-      });
+    styles = StyleSheet.create({
+        barWrapper: {
+            // borderTopWidth: 1,
+            backgroundColor: '#D3D5DB',
+            // borderColor: '#E5E5E5',
+            position: 'absolute',
+            alignSelf: 'stretch',
+            left: 0,
+            right: 0,
+        },
+        bar: {
+            height: 40,
+            alignItems: 'flex-end'
+        },
+        button: {
+            flex: 1,
+            justifyContent: 'center'
+        },
+        done: {
+            fontSize: 21,
+            color: '#218BFE',
+            fontWeight: '500',
+            margin: 10
+        }
     });
-  }
 
-  render() {
-    if (Platform.OS !== 'ios') {
-      return null;
+    componentWillMount(props) {
+        const config = {
+            duration: 250,
+            update: {
+                duration: 250,
+                type: LayoutAnimation.Types.keyboard
+            }
+        };
+
+        if (props.borderColor) {
+            this.styles.barWrapper.borderTopWidth = 1;
+            this.styles.barWrapper.borderColor = props.borderColor;
+        }
+        if (props.backgroundColor) {
+            this.styles.barWrapper.backgroundColor = props.backgroundColor;
+        }
+
+        this.keyboardWillChangeFrameListener = Keyboard.addListener('keyboardWillChangeFrame', ({ endCoordinates }) => {
+            let { screenY } = endCoordinates;
+
+            if (screenY === height || this.props.keyboardType !== 'numeric') {
+                bottom = -81;
+            } else {
+                bottom = endCoordinates.height - 40;
+            }
+
+            this.props.includeLayoutAnimation ? LayoutAnimation.configureNext(config) : null;
+            this.setState({
+                bottom,
+                width: endCoordinates.width
+            });
+        });
     }
 
-    let { bottom, width } = this.state;
+    render() {
+        if (Platform.OS !== 'ios') {
+            return null;
+        }
 
-    return (
-      <View style={[{ bottom, width }, styles.barWrapper]}>
-        <View
-          style={styles.bar}
-          >
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              Keyboard.dismiss();
-              this.props.onDone();
-            }}
-            >
-            <Text style={styles.done}>{this.props.text}</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.bar} />
-      </View>
-    );
-  }
+        let { bottom, width } = this.state;
 
-  componentWillUnmount() {
-    this.keyboardWillChangeFrameListener.remove();
-  }
+        return (
+            <View style={[{ bottom, width }, this.styles.barWrapper]}>
+                <View
+                    style={this.styles.bar}
+                >
+                    <TouchableOpacity
+                        style={this.styles.button}
+                        onPress={() => {
+                            Keyboard.dismiss();
+                            this.props.onDone();
+                        }}
+                    >
+                        <Text style={styles.done}>{this.props.text}</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={this.styles.bar} />
+            </View>
+        );
+    }
+
+    componentWillUnmount() {
+        this.keyboardWillChangeFrameListener.remove();
+    }
 }
-
-const styles = StyleSheet.create({
-  barWrapper: {
-    borderTopWidth: 1,
-    backgroundColor: '#F6F8F9',
-    borderColor: '#E5E5E5',
-    position: 'absolute'
-  },
-  bar: {
-    height: 40,
-    alignItems: 'flex-end'
-  },
-  button: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  done: {
-    fontSize: 21,
-    color: '#218BFE',
-    fontWeight: '500',
-    margin: 10
-  }
-});
 
 export default DoneBar;
